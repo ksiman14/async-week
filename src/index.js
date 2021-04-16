d94c87ed49137df119ca0180c026f32a424391eb
Kelsey Siman <ksiman14@gmail.com>
2021-04-16 09:46:38 -0400

    functional seaweed and nets

--- src/index.js
+++ src/index.js
@@ -10,21 +10,19 @@ class MyGame extends Phaser.Scene {
     super();
     this.ocean;
     this.turtle;
+    this.seaweed;
+    this.nets;
     this.cursors;
+    this.score = 0;
+    this.scoreText;
   }
 
   preload() {
     this.load.image('sky', skyImg);
     this.load.image('water', waterImg);
     this.load.image('turtle', turtleImg);
-    this.load.spritesheet('seaweed', seaweedImg, {
-      frameWidth: 32,
-      frameHeight: 48,
-    });
-    this.load.spritesheet('fishing-net', netImg, {
-      frameWidth: 32,
-      frameHeight: 48,
-    });
+    this.load.image('seaweed', seaweedImg);
+    this.load.image('fishing-net', netImg);
   }
 
   create() {
@@ -36,6 +34,28 @@ class MyGame extends Phaser.Scene {
 
     this.turtle = this.physics.add.sprite(100, 500, 'turtle');
     this.turtle.setCollideWorldBounds = true;
+
+    this.scoreText = this.add.text(16, 16, 'score: 0', {
+      fontSize: '32px',
+      fill: '#000',
+    });
+
+    this.seaweed = this.physics.add.group({
+      key: 'seaweed',
+      repeat: 3,
+      setXY: { x: 50, y: 300, stepX: 200 },
+    });
+
+    this.physics.add.overlap(
+      this.turtle,
+      this.seaweed,
+      this.collectSeaweed,
+      null,
+      this
+    );
+
+    this.nets = this.physics.add.group();
+    this.physics.add.collider(this.turtle, this.nets, this.hitNet, null, this);
   }
 
   update() {
@@ -43,9 +63,42 @@ class MyGame extends Phaser.Scene {
       this.turtle.setVelocityY(-200);
     } else if (this.cursors.down.isDown) {
       this.turtle.setVelocityY(200);
+    } else if (this.cursors.right.isDown) {
+      this.turtle.setVelocityX(200);
+    } else if (this.cursors.left.isDown) {
+      this.turtle.setVelocityX(-200);
     } else {
-      this.turtle.setVelocityY(0);
+      this.turtle.setVelocity(0);
     }
+
+    this.seaweed.children.iterate(function (child) {
+      if (child.y >= 600) {
+        child.disableBody(true, true);
+        child.enableBody(true, child.x, 300, true, true);
+      }
+    });
+  }
+
+  collectSeaweed(player, seaweed) {
+    seaweed.disableBody(true, true);
+    this.score += 10;
+    this.scoreText.setText('Score: ' + this.score);
+
+    const x =
+      this.turtle.x < 400
+        ? Phaser.Math.Between(400, 800)
+        : Phaser.Math.Between(0, 400);
+
+    var net = this.nets.create(x, 16, 'fishing-net');
+    net.setVelocity(Phaser.Math.Between(-200, 200), 20);
+
+    seaweed.enableBody(true, seaweed.x, 300, true, true);
+  }
+
+  hitNet(player, nets) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    this.gameOver = true;
   }
 }
 
@@ -57,7 +110,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { x: 150 },
+      gravity: { y: 150 },
       debug: false,
     },
   },
